82752c9e2047c6b1e92d92cb22807d87e3c93667
Bowen Cui <xcxiongmao@126.com>
2021-12-14 13:18:47 +0800

    update parameters

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # BALL6
 
 BALL6 (B-cell Acute Leukemia Subtype Identification based on rank of eXpression): A deep learning tool for predicting AL and BALL subtypes based on the expression rank values of 455 selected genes.
+
+
+
+Visit our online tool
+
 http://cccg.ronglian.com/#/analysis
+
+
+
+### INSTALL
+
+### USEAGE
+
+>BALL6 **-i** expression.tab [**-o** output_dir] [**-m** moutput_file_prefix]
+      **-i**  A gene expression table file with tab text file contain 2 columns at least.
+              The first column is ENSEMBL ID, and the last column is genes expression value.
+      **-o**  Path to output dir, defult './'
+      **-m**  Prefix for output file, defult 'ball6'
+
--- ball6.sh
+++ ball6.sh
@@ -2,37 +2,74 @@
 ## 2021.11.10
 export PYTHONDONTWRITEBYTECODE=1
 
-FILE=$1
-OUTPUT=$2
+while getopts "i:o:m:" opt
+do
+  case $opt in
+    i)
+    FILE=$OPTARG
+    ;;
+    o)
+    OUTDIR=$OPTARG
+    ;;
+    m)
+    PREFIX=$OPTARG
+    ;;
+    ?)
+    echo -e "usage: \e[1;34mBALL6 \e[1;33m-i \e[1;0mexpression.tab [\e[1;33m-o \e[1;0moutput_dir] [\e[1;33m-m \e[1;0mmoutput_file_prefix]"
+
+    echo -e "  \e[1;33m-i \e[1;0m A gene expression table file with tab text file contain 2 columns at least.\n      The first column is ENSEMBL ID, and the last column is genes expression value."
+    echo -e "  \e[1;33m-o \e[1;0m Path to output dir, defult './'"
+    echo -e "  \e[1;33m-m \e[1;0m Prefix for output file, defult 'ball6'"
+
+    exit 1;;
+  esac
+done
+
+
+if [[ $FILE -eq '' ]]
+then
+  echo -e "ERROR: Parameter -i is necessary (see 'ball6 -h')"
+  exit 1
+fi
+
+
+if [[ $OUTDIR != /* ]];
+then
+ OUTDIR=./$OUTDIR
+fi
+
+if [[ $PREFIX -eq '' ]]
+then
+  PREFIX='ball6'
+fi
+
+# FILE=$1
+OUTPUT=$OUTDIR/$PREFIX
 LOGFILE=${OUTPUT%/*}/log.${OUTPUT##*/}.txt
-# echo file:$FILE
-# echo output:$OUTPUT
-# echo 'LOADING ENV'
-# source activate py39
+# echo $OUTPUT
+# echo $FILE
+# echo $LOGFILE
+
+
+WORKDIR=$(cd "$(dirname "$0")"; pwd)
+
+
+echo 'Wellcome to BALL6!'
+echo 'Visit our online tool http://cccg.ronglian.com/#/analysis'
 
 mkdir -p ${OUTPUT%/*}/
 
 echo 'Running AL Model' > ${LOGFILE} && \
-python /root/pred_al.py ${FILE} $OUTPUT \
+python ${WORKDIR}/pred_al.py ${FILE} $OUTPUT \
   1>> ${LOGFILE} \
   2> /dev/null && \
 echo 'Running BALL Model' >> ${LOGFILE} && \
-python /root/pred_ball.py ${FILE} $OUTPUT \
+python ${WORKDIR}/pred_ball.py ${FILE} $OUTPUT \
   1>> ${LOGFILE} \
   2> /dev/null && \
-python /root/plotPolar.py ${FILE} $OUTPUT && \
+python ${WORKDIR}/plotPolar.py ${FILE} $OUTPUT && \
 echo 'DONE!' >> ${LOGFILE}
 
-chmod 777 ${LOGFILE}
+#chmod 777 ${LOGFILE}
 
-#echo 'PLOTTING Polar' >> ${LOGFILE} && \
-
-#chmod 777 ${OUTPUT%/*}/pydf_al_${OUTPUT##*/}.tab
-#chmod 777 ${OUTPUT%/*}/pydf_ball_${OUTPUT##*/}.tab
-
-# echo 'RUNNING AL model' && \
-    # python /root/pred_al.py ${FILE} $OUTPUT && \
-    # echo 'RUNNING BALL model' && \
-    # python /root/pred_ball.py ${FILE} $OUTPUT && \
-    # echo 'DONE!'
 
